927c35e890333b6644d0a5ebb5c62c7b13b98b42
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2021-01-08 08:45:45 -0400

    Add Zope2 BrowserView Hello World

---
+++ senaite/labmedik/browser/hello.py
@@ -0,0 +1,14 @@
+#from Products.five.browser import BrowserView
+from bika.lims.browser import BrowserView
+
+class HelloWorld(BrowserView):
+    '''
+    Hello World browser view, as simple string
+    '''
+
+    def __init__(self, context, request):
+        self.context = context
+        self.request = request
+    
+    def __call__(self):
+        return "Hello World"
