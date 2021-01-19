54cba884e5e94d1bbbabda2246e883fc09f7abc3
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2021-01-19 17:57:56 -0400

    Updated the senaite.labmedikdemos.helloworld viewlet

--- senaite/labmedikdemos/browser/viewlets.py
+++ senaite/labmedikdemos/browser/viewlets.py
@@ -1,8 +1,10 @@
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from plone.app.layout.viewlets.common import ViewletBase
 
+from senaite.labmedikdemos import labmedikdemosMessageFactory as _
+
 class MyHelloWorldViewlet(ViewletBase):
 	render = ViewPageTemplateFile('myhelloworldviewlet.pt')
 
 	def update(self):
-		self.company = 'Plone Fundation'
+		self.company = _(u'Plone Foundation!!!')
